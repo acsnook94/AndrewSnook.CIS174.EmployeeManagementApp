97da3088b1c3712e791aeccccb3ba09670c81f7b
Andrew Snook <acsnook@dmacc.edu>
2018-10-18 23:54:32 -0500

    Add missing Academic Honesty headers to project files.

--- AndrewSnook.CIS174.EmployeeManagementApp.Api/App_Start/WebApiConfig.cs
+++ AndrewSnook.CIS174.EmployeeManagementApp.Api/App_Start/WebApiConfig.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/** =========================================================
+
+ Andrew Snook
+
+ Operating System: Windows 10
+
+ Microsoft Visual Studio 2017 Enterprise
+
+ CIS 174
+
+ EmployeeManagement (Week 5)
+
+ Program Description: This application allows the user to view and create database records containing employee information
+
+ Academic Honesty:
+
+ I attest that this is my original work.
+
+ I have not used unauthorized source code, either modified or unmodified.
+
+ I have not given other fellow student(s) access to my program.
+
+=========================================================== **/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
--- AndrewSnook.CIS174.EmployeeManagementApp.Shared/Orchestrators/EmployeeOrchestrator.cs
+++ AndrewSnook.CIS174.EmployeeManagementApp.Shared/Orchestrators/EmployeeOrchestrator.cs
@@ -1,4 +1,29 @@
-﻿using AndrewSnook.CIS174.EmployeeManagementApp.Domain;
+﻿/** =========================================================
+
+ Andrew Snook
+
+ Operating System: Windows 10
+
+ Microsoft Visual Studio 2017 Enterprise
+
+ CIS 174
+
+ EmployeeManagement (Week 5)
+
+ Program Description: This application allows the user to view and create database records containing employee information
+
+ Academic Honesty:
+
+ I attest that this is my original work.
+
+ I have not used unauthorized source code, either modified or unmodified.
+
+ I have not given other fellow student(s) access to my program.
+
+=========================================================== **/
+
+
+using AndrewSnook.CIS174.EmployeeManagementApp.Domain;
 using AndrewSnook.CIS174.EmployeeManagementApp.Domain.Entities;
 using AndrewSnook.CIS174.EmployeeManagementApp.Shared.Orchestrators.Interfaces;
 using AndrewSnook.CIS174.EmployeeManagementApp.Shared.ViewModels;
