fc9c3b3e5cab14a82925eca85ed3814e7bb747ce
Andrew Snook <acsnook@dmacc.edu>
2018-10-23 02:56:29 -0500

    --Added Bootstrap menu --Added _Layout.cshtml --Added _ViewStart.cshtml --Added site title --Added copyright footer/section

---
+++ AndrewSnook.CIS174.EmployeeManagementApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AndrewSnook.CIS174.EmployeeManagementApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
