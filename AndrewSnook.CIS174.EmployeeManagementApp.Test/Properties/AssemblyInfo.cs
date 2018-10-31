0157fcd84be74639855d134aade3b4b4c5c32ffa
Andrew Snook <acsnook@dmacc.edu>
2018-10-30 23:17:52 -0500

    -Added new UnitTest project AndrewSnook.CIS174.EmployeeManagementApp.Test -Added AutoMoq to Test project -Added Ninject to Test, Shared projects

---
+++ AndrewSnook.CIS174.EmployeeManagementApp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AndrewSnook.CIS174.EmployeeManagementApp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AndrewSnook.CIS174.EmployeeManagementApp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f9375d0-303d-408e-97f9-619b23b42ba8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
