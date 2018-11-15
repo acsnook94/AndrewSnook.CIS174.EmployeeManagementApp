bc557973ed535ad9390263a83ca9a617dcc4e4d2
Andrew Snook <acsnook@dmacc.edu>
2018-11-15 01:40:13 -0600

    Revert "no message"
    This reverts commit 0a3f2c2cbd535bf827d6b3a03a9f8e2f10812a92.

---
+++ AndrewSnook.CIS174.EmployeeManagementApp.Web/App_Start/IdentityConfig.cs
@@ -0,0 +1,27 @@
+﻿using AndrewSnook.CIS174.EmployeeManagementApp.Domain;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace AndrewSnook.CIS174.EmployeeManagementApp.Web.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(() => new EmployeeContext());
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<RoleManager<AppRole>>((options, context) =>
+                new RoleManager<AppRole>(
+                    new RoleStore<AppRole>(context.Get<EmployeeContext>())));
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login"),
+            });
+        }
+    }
+}
