e9542e2de216a3ccbe7c87e0a7cb5fa249b6d7cc
Andrew Snook <acsnook@dmacc.edu>
2018-11-14 23:24:25 -0600

    Added 2 new Identity classes to Web project: AppUser.cs, AppRole.cs

---
+++ AndrewSnook.CIS174.EmployeeManagementApp.Web/AppRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AndrewSnook.CIS174.EmployeeManagementApp.Web
+{
+    public class AppRole : IdentityRole
+    {
+        public AppRole() : base(){ }
+        public AppRole(string name) : base(name){ }
+        //Put props here
+    }
+}
---
+++ AndrewSnook.CIS174.EmployeeManagementApp.Web/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AndrewSnook.CIS174.EmployeeManagementApp.Web
+{
+    public class AppUser : IdentityUser
+    {
+        //Put props here
+    }
+}
