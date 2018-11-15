0b11889df7a9bd8ebf2ed05bd2e46cb5e42cae86
Andrew Snook <acsnook@dmacc.edu>
2018-11-15 00:03:31 -0600

    Added new classes to Web project: ApplicationDbContext.cs, AppUserManager.cs

---
+++ AndrewSnook.CIS174.EmployeeManagementApp.Web/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AndrewSnook.CIS174.EmployeeManagementApp.Web
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+    }
+}
