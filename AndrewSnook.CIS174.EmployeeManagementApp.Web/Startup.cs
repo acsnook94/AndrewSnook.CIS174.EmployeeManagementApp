d169ff84ece6593e4d323a0135c928b439da083d
Andrew Snook <acsnook@dmacc.edu>
2018-11-15 16:44:50 -0600

    Added "Startup.cs" and "Startup.Auth.cs" (in App_Start) for the "Web" project

---
+++ AndrewSnook.CIS174.EmployeeManagementApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AndrewSnook.CIS174.EmployeeManagementApp.Web.Startup))]
+namespace AndrewSnook.CIS174.EmployeeManagementApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
