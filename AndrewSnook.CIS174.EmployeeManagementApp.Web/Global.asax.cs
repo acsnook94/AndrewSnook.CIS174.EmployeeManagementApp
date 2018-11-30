2cb59786ec72927ab7c871ef8115992cbb85dd59
Andrew Snook <acsnook@dmacc.edu>
2018-11-30 03:05:50 -0600

    Fixed a gigantic reference mess that happened for no apparent reason

--- AndrewSnook.CIS174.EmployeeManagementApp.Web/Global.asax.cs
+++ AndrewSnook.CIS174.EmployeeManagementApp.Web/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using AndrewSnook.CIS174.EmployeeManagementApp.Web.App_Start;
+﻿using AndrewSnook.CIS174.EmployeeManagementApp.Api;
+using AndrewSnook.CIS174.EmployeeManagementApp.Web.App_Start;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -9,6 +11,7 @@ namespace AndrewSnook.CIS174.EmployeeManagementApp.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
