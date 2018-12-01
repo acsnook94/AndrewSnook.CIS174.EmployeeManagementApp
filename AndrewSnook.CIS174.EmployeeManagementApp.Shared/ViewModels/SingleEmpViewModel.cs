6dcb5e60e1e7b25102b5295295352e020a35a737
Andrew Snook <acsnook@dmacc.edu>
2018-11-30 19:18:17 -0600

    --Added new BiewModel "SingleEmpViewModel.cs"

---
+++ AndrewSnook.CIS174.EmployeeManagementApp.Shared/ViewModels/SingleEmpViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AndrewSnook.CIS174.EmployeeManagementApp.Shared.ViewModels
+{
+    public class SingleEmpViewModel
+    {
+        public string EmpFirst {get; set; }
+        public string EmpMiddle {get; set; }
+        public string EmpLast {get; set; }
+        public DateTime? EmpBirthDate {get; set;}
+        public string EmpDept {get; set; }
+    }
+}
